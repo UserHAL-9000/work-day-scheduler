d6758c59addd5487a27a6d829e5c9cc903fbc8b6
UserHAL-9000 <jonconnorrobinson94@gmail.com>
2023-01-30 13:47:40 -0800

    added persistance based off of local storage key values

--- Develop/script.js
+++ Develop/script.js
@@ -24,7 +24,15 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    
+    $('#9 .description').val(localStorage.getItem('9'))
+    $('#10 .description').val(localStorage.getItem('10'))
+    $('#11 .description').val(localStorage.getItem('11'))
+    $('#12 .description').val(localStorage.getItem('12'))
+    $('#13 .description').val(localStorage.getItem('13'))
+    $('#14 .description').val(localStorage.getItem('14'))
+    $('#15 .description').val(localStorage.getItem('15'))
+    $('#16 .description').val(localStorage.getItem('16'))
+    $('#17 .description').val(localStorage.getItem('17'))
     // TODO: Add code to display the current date in the header of the page.
   });
   
